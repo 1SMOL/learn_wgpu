7a871ca69453ba35720abb822ab3fa5f2ffe480f
1SMOL <93025618+1SMOL@users.noreply.github.com>
2022-11-06 15:07:16 -0500

    added readme and inline notes.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn wgpu
+
+This is a simple repo that will follow a native first implementation of the wgpu rust lib. The tutorial that I have followed while excluding all the WASM code is viewable on YouTube [here](https://youtu.be/knmuobQFNmM?list=PLWtPciJ1UMuBs_3G-jFrMJnM5ZMKgl37H).
--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,19 @@ pub fn run() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    /*
+    NOTE:
+        Winit's event_loop can be hard to read, but if you already understand basic pattern matching in rust, this is just that, but nested.
+        If you are any familiar with languages that use '{}', just follow those to see the flow of the match.
+    */
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
+        } if window_id == window.id() => match event { // Here's your nested pattern matching.
             WindowEvent::CloseRequested
             | WindowEvent::KeyboardInput {
+                // You're just checking for the use of the 'esc' key on your keyboard.
                 input:
                     KeyboardInput {
                         state: ElementState::Pressed,
